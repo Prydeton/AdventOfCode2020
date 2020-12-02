ba7bd6a9230e3aff7d99bdc66cc406eb668c8c80
Prydeton <contact@maxwellreid.tech>
2020-12-02 18:14:59 +1100

    Fix spelling of required part 1

--- 02-12-2020/02-12-2020-P1.rb
+++ 02-12-2020/02-12-2020-P1.rb
@@ -13,13 +13,13 @@ values = read_values()
 valid_passwords = 0
 for instance in values
     password_details = instance.split(' ')
-    rqeuired_character = password_details[1].tr!(':', '')
+    required_character = password_details[1].tr!(':', '')
     password = password_details[2]
     min = password_details[0].split('-')[0].to_i()
     max = password_details[0].split('-')[1].to_i()
     count = 0
     password.each_char() { |character|
-        if character == rqeuired_character
+        if character == required_character
             count += 1
         end
     }
