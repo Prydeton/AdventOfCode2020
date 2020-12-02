ac33a356b22567f234ab237da63ca496043ae94a
Prydeton <contact@maxwellreid.tech>
2020-12-02 18:12:00 +1100

    Part 1 complete

---
+++ 02-12-2020/02-12-2020-P1.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+def read_values
+    values = Array.new
+    file = File.open("values.txt").read
+    file.each_line do |line|
+        values.push(line)
+    end
+    return values
+end
+
+values = read_values()
+valid_passwords = 0
+for instance in values
+    password_details = instance.split(' ')
+    rqeuired_character = password_details[1].tr!(':', '')
+    password = password_details[2]
+    min = password_details[0].split('-')[0].to_i()
+    max = password_details[0].split('-')[1].to_i()
+    count = 0
+    password.each_char() { |character|
+        if character == rqeuired_character
+            count += 1
+        end
+    }
+    if count >= min and count <= max
+        valid_passwords += 1
+    end
+end
+puts valid_passwords
